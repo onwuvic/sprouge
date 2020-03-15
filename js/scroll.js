4ffa56bbbd244730df1f633d853f0d9b48933db0
Onwuzor Victor <victor.onwuzor@andela.com>
2020-03-15 08:43:11 +0100

    add hero one section

--- js/scroll.js
+++ js/scroll.js
@@ -3,89 +3,89 @@ window.sr = ScrollReveal();
 			//duration: 2000,
 			//origin: 'bottom'
 		//});
-		sr.reveal('.hero-one', {
-			duration: 2000,
-			origin: 'top',
-			distance: '300px'
-		});
-		sr.reveal('.buttons', {
-			duration: 2000,
-			delay: 1000,
-			origin: 'bottom'
-		});
-		sr.reveal('.hero-two-animate', {
-			duration: 2000,
-			origin: 'right',
-			distance: '300px',
-			viewFactor: 0.4
-		});
-		sr.reveal('.hero-two img', {
-			duration: 2000,
-			origin: 'bottom',
-			distance: '300px',
-			viewFactor: 0.4
-		});
-		sr.reveal('.hero-three-animate', {
-			duration: 2000,
-			origin: 'left',
-			distance: '300px',
-			viewFactor: 0.4
-		});
-		sr.reveal('.hero-three img', {
-			duration: 2000,
-			origin: 'right',
-			distance: '300px',
-			viewFactor: 0.4
-		});
-		sr.reveal('.hero-four-animate', {
-			duration: 2000,
-			origin: 'right',
-			distance: '300px',
-			viewFactor: 0.4
-		});
-		sr.reveal('.hero-four img', {
-			duration: 2000,
-			origin: 'left',
-			distance: '300px',
-			viewFactor: 0.4
-		});
-		sr.reveal('.hero-five-animate', {
-			duration: 2000,
-			origin: 'top',
-			viewFactor: 0.4
-		});
-		sr.reveal('.hero-five-image', {
-			duration: 2000,
-			origin: 'bottom',
-			distance: '300px',
-			viewFactor: 0.1
-		});
-		sr.reveal('.hero-six-animate', {
-			duration: 2000,
-			origin: 'left',
-			distance: '300px',
-			viewFactor: 0.4
-		});
-		sr.reveal('.hero-six img', {
-			duration: 2000,
-			origin: 'right',
-			distance: '300px',
-			viewFactor: 0.2
-		});
-		sr.reveal('.hero-seven-animate', {
-			duration: 2000,
-			origin: 'left',
-			distance: '300px',
-			viewFactor: 0.4
-		});
-		sr.reveal('.hero-seven img', {
-			duration: 2000,
-			origin: 'bottom',
-			distance: '300px',
-			viewFactor: 0.2
-		});
-		sr.reveal('.message-button', {
-			duration: 2000,
-			delay: 1000,
-			origin: 'bottom',
-		});
+sr.reveal('.header__text', {
+	duration: 2000,
+	origin: 'top',
+	distance: '300px'
+});
+sr.reveal('.header__buttons', {
+	duration: 2000,
+	delay: 1000,
+	origin: 'bottom'
+});
+sr.reveal('.hero--one-animate', { 
+	duration: 2000,
+	origin: 'right',
+	distance: '300px',
+	viewFactor: 0.4
+});
+sr.reveal('.hero--one-img', {
+	duration: 2000,
+	origin: 'bottom',
+	distance: '300px',
+	viewFactor: 0.4
+});
+sr.reveal('.hero-three-animate', {
+	duration: 2000,
+	origin: 'left',
+	distance: '300px',
+	viewFactor: 0.4
+});
+sr.reveal('.hero-three img', {
+	duration: 2000,
+	origin: 'right',
+	distance: '300px',
+	viewFactor: 0.4
+});
+sr.reveal('.hero-four-animate', {
+	duration: 2000,
+	origin: 'right',
+	distance: '300px',
+	viewFactor: 0.4
+});
+sr.reveal('.hero-four img', {
+	duration: 2000,
+	origin: 'left',
+	distance: '300px',
+	viewFactor: 0.4
+});
+sr.reveal('.hero-five-animate', {
+	duration: 2000,
+	origin: 'top',
+	viewFactor: 0.4
+});
+sr.reveal('.hero-five-image', {
+	duration: 2000,
+	origin: 'bottom',
+	distance: '300px',
+	viewFactor: 0.1
+});
+sr.reveal('.hero-six-animate', {
+	duration: 2000,
+	origin: 'left',
+	distance: '300px',
+	viewFactor: 0.4
+});
+sr.reveal('.hero-six img', {
+	duration: 2000,
+	origin: 'right',
+	distance: '300px',
+	viewFactor: 0.2
+});
+sr.reveal('.hero-seven-animate', {
+	duration: 2000,
+	origin: 'left',
+	distance: '300px',
+	viewFactor: 0.4
+});
+sr.reveal('.hero-seven img', {
+	duration: 2000,
+	origin: 'bottom',
+	distance: '300px',
+	viewFactor: 0.2
+});
+sr.reveal('.message-button', {
+	duration: 2000,
+	delay: 1000,
+	origin: 'bottom',
+});
